be0e3090eb63ae9178fd4da0050c4adf56c4cce0
mchl <mchlginga@gmail.com>
2025-07-01 23:25:13 +0800

    update server.js

--- server.js
+++ server.js
@@ -4,16 +4,28 @@ const path = require("path");
 const {parse} = require("url");
 
 const config = require("./config/index.js");
+const logger = require("./logger/logger.js");
 
 const noteFile = path.join(__dirname, "notes", "notes.json");
 const port = config.port;
 
+// ensure file "notes.json" exist
 const ensureNoteFile = async () => {
     try {
         await fs.access(noteFile);
     } catch (error) {
         await fs.mkdir(path.dirname(noteFile), {recursive: true});
         await fs.writeFile(noteFile, "[]");
+    }
+};
+
+// pasrse notes.json
+const parseNotes = async () => {
+    try {
+        return JSON.parse(await fs.readFile(noteFile, "utf-8"));
+    } catch (error) {
+        console.log("Error parsing note file:", error.message);
+        return [];
     }
 };
 
@@ -35,6 +47,7 @@ const parseBody = async (req) => {
     });
 };
 
+// this makes the title case sensitive
 const newTitle = (t) => {
     try {
         return t.trim().toLowerCase().replace(/\s+/g, '-');
@@ -53,13 +66,26 @@ const server = http.createServer (async (req, res) => {
         // GET /notes
         if (method === "GET" && pathname === "/notes") {
             try {
-                const notes = JSON.parse(await fs.readFile(noteFile, "utf-8"));
+                const notes = await parseNotes();
+
+                // check if there is a note
+                if (notes.length <= 0) {
+                    res.writeHead(200, {"Content-Type": "application/json"});
+                    await logger(`GET /notes - No notes found`);
+
+                    return res.end(JSON.stringify({message: "No notes found."}));
+                }
+
+                // success response with all notes
                 res.writeHead (200, {"Content-Type": "application/json"});
+                await logger(`GET /notes - Returned ${notes.length} notes`);
 
                 return res.end(JSON.stringify(notes, null, 2));
             } catch (error) {
                 res.writeHead(500, {"Content-Type": "application/json"});
-                return res.end("Failed to read notes.");
+                await logger(`GET /notes - Error: ${error.message}`);
+
+                return res.end(JSON.stringify({error: `${error.message}`}));
             }
         }
 
@@ -67,29 +93,102 @@ const server = http.createServer (async (req, res) => {
         if (method === "POST" && pathname === "/notes") {
             try {
                 const body = await parseBody(req);
-                const notes = JSON.parse(await fs.readFile(noteFile, "utf-8"));
+                const notes = await parseNotes();
 
-                // check if the note is already exist
+                // check if note with same title already exists
                 const exist = notes.find(note => newTitle(note.title) === newTitle(body.title));
                 if (exist){
                     res.writeHead(409, {"Content-Type": "application/json"});
-                    return res.end("Note is already exist.")
+                    await logger(`POST /notes - Note already exists: "${body.title}"`);
+
+                    return res.end(JSON.stringify({error: "Note is already exist."}));
                 }
 
+                // create new note
                 const newNote = {
                     id: Date.now(),
                     title: body.title,
                     body: body.body
                 };
-
                 notes.push(newNote);
                 await fs.writeFile(noteFile, JSON.stringify(notes, null, 2));
 
+                // success response with created note
                 res.writeHead(201, {"Content-Type": "application/json"});
+                await logger(`POST /notes - Created note: "${body.title}"`);
+
                 return res.end (JSON.stringify(newNote, null, 2));
             } catch (error) {
                 res.writeHead(500, {"Content-Type": "application/json"});
-                return res.end("Failed to create note.");
+                await logger(`POST /notes - Error: ${error.message}`);
+
+                return res.end(JSON.stringify({error: `${error.message}`}));
+            }
+        }
+
+        // PUT /notes
+        if (method === "PUT" && pathname.startsWith("/notes/")) {
+            try {
+                const id = Number(pathname.split('/')[2]);
+                const body = await parseBody(req);
+                const notes = await parseNotes();
+
+                // check note with matching id
+                const matchedNote = notes.find(note => note.id === id);
+                if (!matchedNote) {
+                    res.writeHead(200, {"Content-Type": "application/json"});
+                    await logger(`PUT /notes - Note not found.: "${body.title}"`);
+
+                    return res.end(JSON.stringify({message: "Note not found."}))
+                }
+
+                // update notes
+                matchedNote.title = body.title || notes.title;
+                matchedNote.body = body.body || notes.body;
+                await fs.writeFile(noteFile, JSON.stringify(notes, null, 2));
+
+                // success response with updated note
+                res.writeHead(200, {"Content-Type": "application/json"});  
+                await logger(`PUT /notes - Update note: "${body.title}"`);
+
+                return res.end(JSON.stringify(matchedNote, null, 2));
+
+            } catch (error) {
+                res.writeHead(500, {"Content-Type": "application/json"});
+                await logger(`PUT /notes - Error: ${error.message}`);
+
+                return res.end(JSON.stringify({error: `${error.message}`}));
+            }
+        }
+
+        // DELETE /notes
+        if (method === "DELETE" && pathname.startsWith("/notes/")) {
+            try {
+                const id = Number(pathname.split('/')[2]);
+                const notes = await parseNotes();
+
+                // delete note with matching id
+                const filtered = notes.filter(note => note.id !== id);
+                if (filtered.length === notes.length) {
+                    res.writeHead(404, {"Content-Type": "application/json"});
+                    await logger(`DELETE /notes/${id} - Note not found`);
+
+                    return res.end(JSON.stringify({message: "Note not found"}));
+                }
+
+                // write updated notes
+                await fs.writeFile(noteFile, JSON.stringify(filtered, null, 2));
+
+                // success response with updated notes
+                res.writeHead(200, {"Content-Type": "application/json"});
+                await logger(`DELETE /notes/${id} - Successfully deleted note`);
+
+                res.end(JSON.stringify({message: "Note is susscefully deleted."}));
+            } catch (error) {
+                res.writeHead(500, {"Content-Type": "application/json"});
+                await logger(`DELETE /notes - Error: ${error.message}`);
+
+                return res.end(JSON.stringify({error: `${error.message}`}));
             }
         }
     } catch (error) {
